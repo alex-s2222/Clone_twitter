904606203f13d22c8a3683c304d743397a98325d
Juzo(-.-) <alexs22221@gmail.com>
2025-08-05 01:03:02 +0300

    add login test

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -53,6 +53,16 @@ func TestRegisterInput_Validate(t *testing.T) {
 			err: ErrValidation,
 		},
 		{
+			name: "email not valid",
+			input: RegisterInput{
+				Username:        "bob",
+				Email:           "bobgmail.com",
+				Password:        "password",
+				ConfirmPassword: "password",
+			},
+			err: ErrValidation,
+		},
+		{
 			name: "to short password",
 			input: RegisterInput{
 				Username:        "bob",
@@ -69,6 +79,70 @@ func TestRegisterInput_Validate(t *testing.T) {
 				Email:           "bob@gmail.com",
 				Password:        "password",
 				ConfirmPassword: "password1",
+			},
+			err: ErrValidation,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.input.Validate()
+
+			if tc.err != nil {
+				require.ErrorIs(t, err, tc.err)
+			} else {
+				require.NoError(t, err, tc.err)
+			}
+
+		})
+	}
+}
+
+
+func TestLoginInput_Sanitize(t *testing.T) {
+	input := LoginInput{
+		Email:           "   BOB@gmail.com   ",
+		Password:        "password",
+	}
+
+	want := LoginInput{
+		Email:           "bob@gmail.com",
+		Password:        "password",
+	}
+
+	input.Sanitize()
+
+	require.Equal(t, want, input)
+}
+
+
+func TestLoginInput_Validate(t *testing.T) {
+	testCases := []struct {
+		name  string
+		input LoginInput
+		err   error
+	}{
+		{
+			name: "valid",
+			input: LoginInput{
+				Email:           "bob@gmail.com",
+				Password:        "password",
+			},
+			err: nil,
+		},
+		{
+			name: "not valid emai",
+			input: LoginInput{
+				Email:           "bobgmail.com",
+				Password:        "password",
+			},
+			err: ErrValidation,
+		},
+		{
+			name: "password required",
+			input: LoginInput{
+				Email:           "bob@gmail.com",
+				Password:        "",
 			},
 			err: ErrValidation,
 		},
